d4f3fc9bf8a507f52f1ad609198ccbb8f3be4c33
Patrice Lynde <3510343+dogfootruler-kr@users.noreply.github.com>
2023-07-01 21:51:50 +0900

    Update news api credentials

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import config from "../config";
 
 
 class News extends React.Component {
@@ -11,14 +12,15 @@ class News extends React.Component {
   }
 
   async fetchData() {
-    await fetch(`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${import.meta.env.VITE_NEWS_API_KEY}`)
-    .then((response) => response.json())
-    .then(data => {this.setState({posts: data.articles})
-      console.log(data)
-    })
-    .catch(err => {
-      console.log(err);
-    });
+    await fetch(`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${config.newsapiCredential}`)
+      .then((response) => response.json())
+      .then(data => {
+        this.setState({ posts: data.articles })
+        console.log(data)
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 
   async componentDidMount() {
@@ -26,32 +28,32 @@ class News extends React.Component {
     this.state.isLoaded = true
   }
 
-	render() {
+  render() {
     console.log(this.state.posts)
     if (this.state.isLoaded) {
       return (
         <>
-        <div className="rounded-xl overflow-y-auto overflow-hidden h-80 w-[30.5rem] space-y-0">
-            {this.state.posts.map((category) =>(
+          <div className="rounded-xl overflow-y-auto overflow-hidden h-80 w-[30.5rem] space-y-0">
+            {this.state.posts.map((category) => (
               <div className="rounded-xl h-24 w-full">
-                  <div className="overflow-hidden h-full w-48 float-left">
-                    <img className="w-full max-h-full" src={category.urlToImage}></img>
-                  </div>
-                  <div className="pl-4 text-white overflow-hidden h-full w-72 float-left">
-                    <a className="text-blue-600 hover:text-blue-800 visited:text-purple-600 pt-1 float-right text-[14px]" href={category.url}>{category.title}</a>
-                  </div>
+                <div className="overflow-hidden h-full w-48 float-left">
+                  <img className="w-full max-h-full" src={category.urlToImage}></img>
+                </div>
+                <div className="pl-4 text-white overflow-hidden h-full w-72 float-left">
+                  <a className="text-blue-600 hover:text-blue-800 visited:text-purple-600 pt-1 float-right text-[14px]" href={category.url}>{category.title}</a>
+                </div>
               </div>
             ))}
-        </div>
+          </div>
         </>
       );
     }
     else {
       return (
         <>
-        <div className="text-center items-center justify-center translate-x-0 translate-y-0">
-          <h1>No news.</h1>
-        </div>
+          <div className="text-center items-center justify-center translate-x-0 translate-y-0">
+            <h1>No news.</h1>
+          </div>
         </>
       );
     }
--- src/views/Index.jsx
+++ src/views/Index.jsx
@@ -28,40 +28,40 @@ export default function Index() {
 
           {/* row 1 */}
           <div className="overflow-hidden rounded-xl col-span-1 row-span-2 h-80 w-36 shadow-4xl dark:shadow-none">
-            <div className="sticky rounded-xl overflow-hidden h-80 w-36 border-0 dark:border-4 dark:border-off-white2"> 
-              <video className="relative object-cover min-h-full max-w-xl -left-12" src={ desert } type="video/mp4" autoPlay muted loop/>
+            <div className="sticky rounded-xl overflow-hidden h-80 w-36 border-0 dark:border-4 dark:border-off-white2">
+              <video className="relative object-cover min-h-full max-w-xl -left-12" src={desert} type="video/mp4" autoPlay muted loop />
             </div>
           </div>
           <div className="rounded-xl col-span-1 h-36 w-36 shadow-4xl dark:shadow-none">
-            <div className="sticky rounded-xl overflow-hidden h-36 w-36 border-0 dark:border-4 dark:border-off-white2"> 
-              <video className="relative object-cover min-h-full max-w-sm right-48" src={ desert } type="video/mp4" autoPlay muted loop/>
+            <div className="sticky rounded-xl overflow-hidden h-36 w-36 border-0 dark:border-4 dark:border-off-white2">
+              <video className="relative object-cover min-h-full max-w-sm right-48" src={desert} type="video/mp4" autoPlay muted loop />
             </div>
           </div>
           <div className="bg-blue3 text-black rounded-xl col-span-2 h-36 w-80 shadow-4xl dark:shadow-none border-0 dark:border-4 dark:border-off-white2"><SearchBox /></div>
-          <Bookmark title={ config.bookmark[0].title } content={ config.bookmark[0].content } />
-          <div className="bg-off-white1 text-black rounded-xl col-span-1 h-36 w-36 shadow-4xl dark:shadow-none"><Unsplash search={ config.unsplash.box1 }/></div>
+          <Bookmark title={config.bookmark[0].title} content={config.bookmark[0].content} />
+          <div className="bg-off-white1 text-black rounded-xl col-span-1 h-36 w-36 shadow-4xl dark:shadow-none"><Unsplash search={config.unsplash.box1} /></div>
           <div className="bg-green2 dark:bg-green1 text-black rounded-xl col-span-1 h-36 w-36 shadow-4xl dark:shadow-none border-0 dark:border-4 dark:border-off-white2"><WeatherBox /></div>
-          
+
           {/* row 2 */}
-          <div className="bg-off-white1 text-black rounded-xl col-span-1 h-36 w-36 shadow-4xl dark:shadow-none"><Unsplash search={ config.unsplash.box2 }/></div>
-          <Bookmark title={ config.bookmark[1].title } content={ config.bookmark[1].content } />
+          <div className="bg-off-white1 text-black rounded-xl col-span-1 h-36 w-36 shadow-4xl dark:shadow-none"><Unsplash search={config.unsplash.box2} /></div>
+          <Bookmark title={config.bookmark[1].title} content={config.bookmark[1].content} />
           <div className="overflow-hidden rounded-xl col-span-3 row-span-2 h-80 shadow-4xl dark:shadow-none "><Windy /></div>
-          <div className="bg-off-white1 text-black rounded-xl col-span-1 h-36 w-36 shadow-4xl dark:shadow-none"><Unsplash search={ config.unsplash.box3 }/></div>
+          <div className="bg-off-white1 text-black rounded-xl col-span-1 h-36 w-36 shadow-4xl dark:shadow-none"><Unsplash search={config.unsplash.box3} /></div>
 
           {/* row 3 */}
-          <Bookmark title={ config.bookmark[2].title } content={ config.bookmark[2].content } />
+          <Bookmark title={config.bookmark[2].title} content={config.bookmark[2].content} />
           <div className="bg-[#000000] rounded-xl col-span-2 row-span-2 shadow-4xl dark:shadow-none border-0 dark:border-4 dark:border-off-white2"><SolarGraph /></div>
-          <Bookmark title={ config.bookmark[3].title } content={ config.bookmark[3].content } />
+          <Bookmark title={config.bookmark[3].title} content={config.bookmark[3].content} />
 
           {/* row 4 */}
-          <Bookmark title={ config.bookmark[4].title } content={ config.bookmark[4].content } />
-          <div className="bg-off-white1 text-black rounded-xl col-span-1 h-36 w-36 shadow-4xl dark:shadow-none"><Unsplash search={ config.unsplash.box4 }/></div>
+          <Bookmark title={config.bookmark[4].title} content={config.bookmark[4].content} />
+          <div className="bg-off-white1 text-black rounded-xl col-span-1 h-36 w-36 shadow-4xl dark:shadow-none"><Unsplash search={config.unsplash.box4} /></div>
           <div className="flex items-center justify-center bg-blue3 text-white rounded-xl col-span-1 h-36 w-36 shadow-4xl dark:shadow-none border-0 dark:border-4 dark:border-off-white2">
             <ThemeProvider>
               <Toggle />
             </ThemeProvider>
           </div>
-          <div className="text-black rounded-xl col-span-1 h-36 w-36 shadow-4xl dark:shadow-none"><Unsplash search={ config.unsplash.box5 }/></div>
+          <div className="text-black rounded-xl col-span-1 h-36 w-36 shadow-4xl dark:shadow-none"><Unsplash search={config.unsplash.box5} /></div>
           <div className="bg-red2 dark:bg-red1 text-black rounded-xl col-span-1 h-36 w-36 shadow-4xl dark:shadow-none border-0 dark:border-4 dark:border-off-white2"><Clock /></div>
 
         </div>
